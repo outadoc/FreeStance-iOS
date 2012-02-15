ad8195342e4094066384b3cf771a9b2cad3d759a
outadoc <outadoc@me.com>
2012-02-15 16:31:36 +0100

    NOW we're ready for the apocalypse.

--- Resources/views/subviews/easter_egg.js
+++ Resources/views/subviews/easter_egg.js
@@ -1,36 +1,42 @@
 Ti.include('../includes/lib/json.i18n.js');
+Ti.include('../includes/enums.js');
 
 var win = Titanium.UI.currentWindow;
 
-var movie = Titanium.Media.createVideoPlayer({
-	url:'http://dev.outadoc.fr/freestance/anonymous.mp4',
-	backgroundColor:'#111',
-	scalingMode:Titanium.Media.VIDEO_SCALING_ASPECT_FIT,
-	fullscreen:true
-});
-
-var b_close = Ti.UI.createButton({
-	title:I('buttons.close'),
-	style:Ti.UI.iPhone.SystemButtonStyle.PLAIN
-});
-
-function stopAndClose()
-{
-	if(movie != null) {
-		movie.stop();
+if(Ti.Network.networkType == Ti.Network.NETWORK_NONE) {
+	var alert = Ti.UI.createAlertDialog({
+		title:I('network.message.title'),
+		message:'One does not simply watch an easter egg without any connection.' + ' (E' + Error.NETWORK + ')',
+		buttonNames:[I('network.buttons.ok')]
+	});
+	
+	alert.show();
+	alert.addEventListener('click', function(e) { win.close(); });
+} 
+else {
+	var movie = Titanium.Media.createVideoPlayer({
+		url:'http://dev.outadoc.fr/freestance/anonymous.mp4',
+		backgroundColor:'#111',
+		scalingMode:Titanium.Media.VIDEO_SCALING_ASPECT_FIT
+	});
+	
+	var b_close = Ti.UI.createButton({
+		title:I('buttons.close'),
+		style:Ti.UI.iPhone.SystemButtonStyle.PLAIN
+	});
+	
+	function stopAndClose()
+	{
+		if(movie != null) {
+			movie.stop();
+		}
+		win.close();
 	}
-	win.close();
+	
+	b_close.addEventListener('click', stopAndClose);
+	movie.addEventListener('complete', stopAndClose);
+	
+	win.setLeftNavButton(b_close);
+	win.add(movie);
+	movie.play();
 }
-
-b_close.addEventListener('click', stopAndClose);
-movie.addEventListener('complete', stopAndClose);
-movie.addEventListener('fullscreen', function(e)
-{
-	if(!e.entering) {
-		stopAndClose();
-	}
-});
-
-win.setLeftNavButton(b_close);
-win.add(movie);
-movie.play();
