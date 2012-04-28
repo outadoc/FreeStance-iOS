4466c823a7358d8eb0a6ded26d18c1869bf769b0
outadoc <outadoc@me.com>
2012-04-28 20:11:32 +0200

    That's not what was on the server.

--- server/xmlproxy.php
+++ server/xmlproxy.php
@@ -1,11 +1,24 @@
 <?php
-$count = 1;
-$url = 'http://zap-programme.fr/rss/rss.php?bouquet=2&day=now';
-$xml = file_get_contents($url);
-
-$xml = $xml.replace("<br>", "<br />");
-$xml = $xml.replace("<br/>", "<br />");
-
-header("Content-Type: text/xml");
-echo $xml;
+	$url = null;
+	$count = 1;
+	
+	if(isset($_GET['now'])) {
+		$url = 'http://www.zap-programme.fr/rss/rss.php?bouquet=2&day=now';
+	} else if(isset($_GET['tonight'])) {
+		$url = 'http://programme-tv.orange.fr/rss/fluxRssProgrammeSoiree.xml';
+	}
+	
+	$xml = file_get_contents($url);
+	
+	if(isset($_GET['now'])) {
+		$xml = delreturn($xml, $count);
+	}
+	
+	header("Content-Type: text/xml");
+	echo $xml;
+	
+	function delreturn($str, $count) {
+	   $str = preg_replace("/(\r\n|\n|\r)/", "", $str, $count);
+	   return preg_replace("=<br */?>=i", "\n", $str, $count);
+	}
 ?>
