68e32348d8e2380c67a4af87bd09a6932b42b326
outadoc <outadoc@me.com>
2012-06-18 13:21:37 +0200

    Enwrapped app.js in an anonymous function for memory reasons + quick fix for motd throwing a warning when in debug perspective

--- Resources/app.js
+++ Resources/app.js
@@ -1,146 +1,149 @@
-Ti.include('/includes/lib/json.i18n.js');
-Ti.include('/includes/utils.js');
-Ti.include('/includes/ui.js');
-Ti.include('/includes/enums.js');
+(function() {
+	Ti.include('/includes/lib/json.i18n.js');
+	Ti.include('/includes/utils.js');
+	Ti.include('/includes/ui.js');
+	Ti.include('/includes/enums.js');
 
-var tabGroup = Ti.UI.createTabGroup();
+	var tabGroup = Ti.UI.createTabGroup();
 
-var win1 = Ti.UI.createWindow({
-	url: 'views/main.js',
-	backgroundColor: '#323232',
-	navBarHidden: true,
-	orientationModes: [Ti.UI.PORTRAIT],
-	barColor: '#464646'
-});
-
-var win2 = Ti.UI.createWindow({
-	url: 'views/mosaic.js',
-	barColor: '#464646',
-	backgroundColor: '#323232',
-	orientationModes: [Ti.UI.PORTRAIT]
-});
-
-var win3 = Ti.UI.createWindow({
-	url: 'views/epg.js',
-	backgroundColor: '#fff',
-	barColor: '#464646',
-	orientationModes: [Ti.UI.PORTRAIT]
-});
-
-var win4 = Ti.UI.createWindow({
-	title: I('labels.more'),
-	url: 'views/more.js',
-	barColor: '#464646',
-	backgroundColor: getDefaultBackground(),
-	orientationModes: [Ti.UI.PORTRAIT]
-});
-
-//add tabs
-var tab1 = Ti.UI.createTab({
-	icon: '/img/remote.png',
-	title: Ti.App.name,
-	window: win1
-});
-
-var tab2 = Ti.UI.createTab({
-	icon: '/img/planet.png',
-	title: I('labels.mosaic'),
-	window: win2
-});
-
-var tab3 = Ti.UI.createTab({
-	icon: '/img/tv.png',
-	title: I('labels.epg'),
-	window: win3
-});
-
-var tab4 = Ti.UI.createTab({
-	icon: 'img/gear.png',
-	title: I('labels.more'),
-	window: win4
-});
-
-tabGroup.addTab(tab1);
-tabGroup.addTab(tab2);
-tabGroup.addTab(tab3);
-tabGroup.addTab(tab4);
-
-Ti.UI.setOrientation(Ti.UI.PORTRAIT);
-tabGroup.open();
-
-if (!Ti.App.Properties.getBool('hasBeenSet', false)) {
-	var alert = Ti.UI.createAlertDialog({
-		title: I('welcome.message.title'),
-		message: I('welcome.message.message', Ti.App.name),
-		buttonNames: [I('welcome.buttons.no'), I('welcome.buttons.yes')],
-		cancel: 0
+	var win1 = Ti.UI.createWindow({
+		url: 'views/main.js',
+		backgroundColor: '#323232',
+		navBarHidden: true,
+		orientationModes: [Ti.UI.PORTRAIT],
+		barColor: '#464646'
 	});
 
-	alert.show();
-
-	alert.addEventListener('click', function(e) {
-		if (e.index == 1) {
-			var helpwin = Ti.UI.createWindow({
-				url: 'views/subviews/options/options.js',
-				title: I('labels.options'),
-				backgroundColor: 'stripped',
-				barColor: '#464646'
-			});
-
-			tab4.open(helpwin);
-			tabGroup.setActiveTab(3);
-		}
-	});
-}
-
-if (Ti.Network.networkType != Ti.Network.NETWORK_WIFI) {
-	var alert = Ti.UI.createAlertDialog({
-		title: I('network.message.title'),
-		message: I('network.message.message', Ti.App.name) + ' (E' + Error.NETWORK + ')',
-		buttonNames: [I('network.buttons.ok')]
+	var win2 = Ti.UI.createWindow({
+		url: 'views/mosaic.js',
+		barColor: '#464646',
+		backgroundColor: '#323232',
+		orientationModes: [Ti.UI.PORTRAIT]
 	});
 
-	alert.show();
-} else {
-	var xhr = Ti.Network.createHTTPClient({
-		onload: function() {
-			var motd = this.responseText;
-			motd = JSON.parse(motd);
+	var win3 = Ti.UI.createWindow({
+		url: 'views/epg.js',
+		backgroundColor: '#fff',
+		barColor: '#464646',
+		orientationModes: [Ti.UI.PORTRAIT]
+	});
 
-			if (motd != null) {
+	var win4 = Ti.UI.createWindow({
+		title: I('labels.more'),
+		url: 'views/more.js',
+		barColor: '#464646',
+		backgroundColor: getDefaultBackground(),
+		orientationModes: [Ti.UI.PORTRAIT]
+	});
+
+	//add tabs
+	var tab1 = Ti.UI.createTab({
+		icon: '/img/remote.png',
+		title: Ti.App.name,
+		window: win1
+	});
+
+	var tab2 = Ti.UI.createTab({
+		icon: '/img/planet.png',
+		title: I('labels.mosaic'),
+		window: win2
+	});
+
+	var tab3 = Ti.UI.createTab({
+		icon: '/img/tv.png',
+		title: I('labels.epg'),
+		window: win3
+	});
+
+	var tab4 = Ti.UI.createTab({
+		icon: 'img/gear.png',
+		title: I('labels.more'),
+		window: win4
+	});
+
+	tabGroup.addTab(tab1);
+	tabGroup.addTab(tab2);
+	tabGroup.addTab(tab3);
+	tabGroup.addTab(tab4);
+
+	Ti.UI.setOrientation(Ti.UI.PORTRAIT);
+	tabGroup.open();
+
+	if(!Ti.App.Properties.getBool('hasBeenSet', false)) {
+		var alert = Ti.UI.createAlertDialog({
+			title: I('welcome.message.title'),
+			message: I('welcome.message.message', Ti.App.name),
+			buttonNames: [I('welcome.buttons.no'), I('welcome.buttons.yes')],
+			cancel: 0
+		});
+
+		alert.show();
+
+		alert.addEventListener('click', function(e) {
+			if(e.index == 1) {
+				var helpwin = Ti.UI.createWindow({
+					url: 'views/subviews/options/options.js',
+					title: I('labels.options'),
+					backgroundColor: 'stripped',
+					barColor: '#464646'
+				});
+
+				tab4.open(helpwin);
+				tabGroup.setActiveTab(3);
+			}
+		});
+	}
+
+	if(Ti.Network.networkType != Ti.Network.NETWORK_WIFI) {
+		var alert = Ti.UI.createAlertDialog({
+			title: I('network.message.title'),
+			message: I('network.message.message', Ti.App.name) + ' (E' + Error.NETWORK + ')',
+			buttonNames: [I('network.buttons.ok')]
+		});
+
+		alert.show();
+	} else {
+		var xhr = Ti.Network.createHTTPClient({
+			onload: function() {
 				try {
-					if (Ti.App.Properties.getString('lastMotd') != motd.id && Ti.App.getVersion() != motd.lastVersion) {
-						var motdAlert = Ti.UI.createAlertDialog({
-							title: motd.title,
-							message: motd.message,
-							buttonNames: [motd.buttons.cancel, motd.buttons.accept],
-							cancel: 0
-						});
-						motdAlert.show();
+					var motd = this.responseText;
+					motd = JSON.parse(motd);
 
-						motdAlert.addEventListener('click', function(e) {
-							if (e.index == 1 && motd.url !== null) {
-								var w = Ti.UI.createWindow({
-									url: 'views/subviews/website.js',
-									isModalWin: true,
-									thisUrl: motd.url,
-									barColor: '#464646'
-								});
+					if(motd != null) {
+						if(Ti.App.Properties.getString('lastMotd') != motd.id && Ti.App.getVersion() != motd.lastVersion) {
+							var motdAlert = Ti.UI.createAlertDialog({
+								title: motd.title,
+								message: motd.message,
+								buttonNames: [motd.buttons.cancel, motd.buttons.accept],
+								cancel: 0
+							});
+							motdAlert.show();
 
-								w.open({
-									modal: true
-								});
-							}
-						});
+							motdAlert.addEventListener('click', function(e) {
+								if(e.index == 1 && motd.url !== null) {
+									var w = Ti.UI.createWindow({
+										url: 'views/subviews/website.js',
+										isModalWin: true,
+										thisUrl: motd.url,
+										barColor: '#464646'
+									});
 
-						Ti.App.Properties.setString('lastMotd', motd.id);
+									w.open({
+										modal: true
+									});
+								}
+							});
+
+							Ti.App.Properties.setString('lastMotd', motd.id);
+						}
+
 					}
 				} catch(e) {
 				}
 			}
-		}
-	});
+		});
 
-	xhr.open('GET', 'http://apps.outadoc.fr/freestance/motd.json');
-	xhr.send();
-}
+		xhr.open('GET', 'http://apps.outadoc.fr/freestance/motd.json');
+		xhr.send();
+	}
+})();
