6eb02ffc0b3b57467d5165ef180f13b5bff5f8e3
outadoc <outadoc@me.com>
2012-09-22 15:58:22 +0200

    Even better handling of the iPhone 5

--- Resources/views/mosaic.js
+++ Resources/views/mosaic.js
@@ -52,7 +52,7 @@ loadingWin.open();
 var dashboardTNT = Ti.UI.createDashboardView({
 	editable: false,
 	top: 0,
-	height: Ti.UI.FILL
+	height: (Ti.Platform.displayCaps.platformHeight < 568) ? 350 : 430
 });
 
 for(var i = 0; i < labelsTNT.length; i++) {
@@ -66,7 +66,7 @@ win.add(dashboardTNT);
 var dashboardFree = Ti.UI.createDashboardView({
 	editable: false,
 	top: 0,
-	height: Ti.UI.FILL,
+	height: (Ti.Platform.displayCaps.platformHeight < 568) ? 350 : 430,
 	visible: false
 });
 
@@ -81,7 +81,7 @@ win.add(dashboardFree);
 var dashboardCanal = Ti.UI.createDashboardView({
 	editable: false,
 	top: 0,
-	height: Ti.UI.FILL,
+	height: (Ti.Platform.displayCaps.platformHeight < 568) ? 350 : 430,
 	visible: false
 });
 
