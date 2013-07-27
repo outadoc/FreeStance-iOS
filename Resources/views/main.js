814dc1505a658c5384e71306e8fa1f1ea9474457
outadoc <outadoc@gmail.com>
2013-07-27 20:42:41 +0200

    Update buttons for iPad + add trackpad

--- Resources/views/main.js
+++ Resources/views/main.js
@@ -35,51 +35,135 @@
 		width: '300',
 		backgroundColor: Ui.getBarColor()
 	}),
-
-	//the buttons and their placing
-	/*@formatter:off*/
-	buttonList = [
-		{id: 'red', height: 35, width: 35, left: 37, top: 25, canBeLong: false, isColor: true},
-		{id: 'up', height: 40, width: 40, left: 77, top: 20, canBeLong: false, isArrow: true},
-		{id: 'blue', height: 35, width: 35, left: 122, top: 25, canBeLong: false, isColor: true},
-		{id: 'left', height: 40, width: 40, left: 32, top: 65, canBeLong: false, isArrow: true},
-		{title: I('buttons.ok'), id: 'ok', height: 40, width: 40, left: 77, top: 65, canBeLong: false},
-		{id: 'right', height: 40, width: 40, left: 122, top: 65, canBeLong: false, isArrow: true},
-		{id: 'green', height: 35, width: 35, left: 37, top: 110, canBeLong: false, isColor: true},
-		{id: 'down', height: 40, width: 40, left: 77, top: 110, canBeLong: false, isArrow: true},
-		{id: 'yellow', height: 35, width: 35, left: 122, top: 110, canBeLong: false, isColor: true},
-		{title: '+', id: 'vol_inc', height: 50, width: 40, left: 200, top: 23, bFontSize: 23, canRepeat: true},
-		{title: '-', id: 'vol_dec', height: 50, width: 40, left: 200, top: 93, bFontSize: 28, canRepeat: true},
-		{title: '+', id: 'prgm_inc', height: 50, width: 40, left: 255, top: 23, bFontSize: 23, canRepeat: false},
-		{title: '-', id: 'prgm_dec', height: 50, width: 40, left: 255, top: 93, bFontSize: 28, canRepeat: false},
-		{title: '1', id: '1', height: 35, width: 45, left: 20, top: 175, canBeLong: true},
-		{title: '2', id: '2', height: 35, width: 45, left: 75, top: 175, canBeLong: true},
-		{title: '3', id: '3', height: 35, width: 45, left: 130, top: 175, canBeLong: true},
-		{title: '4', id: '4', height: 35, width: 45, left: 20, top: 220, canBeLong: true},
-		{title: '5', id: '5', height: 35, width: 45, left: 75, top: 220, canBeLong: true},
-		{title: '6', id: '6', height: 35, width: 45, left: 130, top: 220, canBeLong: true},
-		{title: '7', id: '7', height: 35, width: 45, left: 20, top: 265, canBeLong: true},
-		{title: '8', id: '8', height: 35, width: 45, left: 75, top: 265, canBeLong: true},
-		{title: '9', id: '9', height: 35, width: 45, left: 130, top: 265, canBeLong: true},
-		{title: ' ←', id: 'back', height: 35, width: 45, left: 20, top: 310, bFontSize: 23, canBeLong: false},
-		{title: '0', id: '0', height: 35, width: 45, left: 75, top: 310, canBeLong: true },
-		{title: '↻', id: 'swap', height: 35, width: 45, left: 130, top: 310, canBeLong: true},
-		{id: 'power', height: 35, width: 95, left: 200, top: 175, canBeLong: false},
-		{id: 'home', height: 35, width: 95, left: 200, top: 220, canBeLong: true},
-		{title: I('buttons.mute'), id: 'mute', height: 35, width: 95, left: 200, top: 265, canBeLong: false},
-		{title: '...', id: 'other', height: 35, width: 95, left: 200, top: 310, canBeLong: false}
-	];
-	/*@formatter:on*/
 	
-	if(Ti.Platform.displayCaps.platformHeight >= 568) {
-		buttonList.push(
-			{title: I('buttons.list'), id: 'list', height: 30, width: 85, bottom: 60, left: 20, bFontSize: 15, canBeLong: false},
-			{title: I('buttons.hdd'), id: 'mail', height: 30, width: 85, bottom: 60, left: 115, bFontSize: 15, canBeLong: false, isLong: true},
-			{title: I('buttons.mail'), id: 'mail', height: 30, width: 85, bottom: 60, left: 210, bFontSize: 15, canBeLong: false},
-			{title: I('buttons.epg'), id: 'epg', height: 30, width: 85, bottom: 20, left: 20, bFontSize: 15, canBeLong: false},
-			{title: I('buttons.pip'), id: 'pip', height: 30, width: 85, bottom: 20, left: 115, bFontSize: 15, canBeLong: false},
-			{title: I('buttons.aux'), id: 'tv', height: 30, width: 85, bottom: 20, left: 210, bFontSize: 15, canBeLong: false}
-		);
+	buttonList = [];
+	
+	if(!Utils.isiPad()) {
+		//the buttons and their placing
+		/*@formatter:off*/
+		buttonList = [
+			{id: 'red', height: 35, width: 35, left: 37, top: 25, canBeLong: false, isColor: true},
+			{id: 'up', height: 40, width: 40, left: 77, top: 20, canBeLong: false, isArrow: true},
+			{id: 'blue', height: 35, width: 35, left: 122, top: 25, canBeLong: false, isColor: true},
+			{id: 'left', height: 40, width: 40, left: 32, top: 65, canBeLong: false, isArrow: true},
+			{title: I('buttons.ok'), id: 'ok', height: 40, width: 40, left: 77, top: 65, canBeLong: false},
+			{id: 'right', height: 40, width: 40, left: 122, top: 65, canBeLong: false, isArrow: true},
+			{id: 'green', height: 35, width: 35, left: 37, top: 110, canBeLong: false, isColor: true},
+			{id: 'down', height: 40, width: 40, left: 77, top: 110, canBeLong: false, isArrow: true},
+			{id: 'yellow', height: 35, width: 35, left: 122, top: 110, canBeLong: false, isColor: true},
+			{title: '+', id: 'vol_inc', height: 50, width: 40, left: 200, top: 23, bFontSize: 23, canRepeat: true},
+			{title: '-', id: 'vol_dec', height: 50, width: 40, left: 200, top: 93, bFontSize: 28, canRepeat: true},
+			{title: '+', id: 'prgm_inc', height: 50, width: 40, left: 255, top: 23, bFontSize: 23, canRepeat: false},
+			{title: '-', id: 'prgm_dec', height: 50, width: 40, left: 255, top: 93, bFontSize: 28, canRepeat: false},
+			{title: '1', id: '1', height: 35, width: 45, left: 20, top: 175, canBeLong: true},
+			{title: '2', id: '2', height: 35, width: 45, left: 75, top: 175, canBeLong: true},
+			{title: '3', id: '3', height: 35, width: 45, left: 130, top: 175, canBeLong: true},
+			{title: '4', id: '4', height: 35, width: 45, left: 20, top: 220, canBeLong: true},
+			{title: '5', id: '5', height: 35, width: 45, left: 75, top: 220, canBeLong: true},
+			{title: '6', id: '6', height: 35, width: 45, left: 130, top: 220, canBeLong: true},
+			{title: '7', id: '7', height: 35, width: 45, left: 20, top: 265, canBeLong: true},
+			{title: '8', id: '8', height: 35, width: 45, left: 75, top: 265, canBeLong: true},
+			{title: '9', id: '9', height: 35, width: 45, left: 130, top: 265, canBeLong: true},
+			{title: ' ←', id: 'back', height: 35, width: 45, left: 20, top: 310, bFontSize: 23, canBeLong: false},
+			{title: '0', id: '0', height: 35, width: 45, left: 75, top: 310, canBeLong: true },
+			{title: '↻', id: 'swap', height: 35, width: 45, left: 130, top: 310, canBeLong: true},
+			{id: 'power', height: 35, width: 95, left: 200, top: 175, canBeLong: false},
+			{id: 'home', height: 35, width: 95, left: 200, top: 220, canBeLong: true},
+			{title: I('buttons.mute'), id: 'mute', height: 35, width: 95, left: 200, top: 265, canBeLong: false},
+			{title: '...', id: 'other', height: 35, width: 95, left: 200, top: 310, canBeLong: false}
+		];
+		/*@formatter:on*/
+		
+		if(Ti.Platform.displayCaps.platformHeight >= 568) {
+			buttonList.push(
+				{title: I('buttons.list'), id: 'list', height: 30, width: 85, bottom: 60, left: 20, bFontSize: 15, canBeLong: false},
+				{title: I('buttons.hdd'), id: 'mail', height: 30, width: 85, bottom: 60, left: 115, bFontSize: 15, canBeLong: false, isLong: true},
+				{title: I('buttons.mail'), id: 'mail', height: 30, width: 85, bottom: 60, left: 210, bFontSize: 15, canBeLong: false},
+				{title: I('buttons.epg'), id: 'epg', height: 30, width: 85, bottom: 20, left: 20, bFontSize: 15, canBeLong: false},
+				{title: I('buttons.pip'), id: 'pip', height: 30, width: 85, bottom: 20, left: 115, bFontSize: 15, canBeLong: false},
+				{title: I('buttons.aux'), id: 'tv', height: 30, width: 85, bottom: 20, left: 210, bFontSize: 15, canBeLong: false}
+			);
+		}
+	} else {//+175
+		buttonList = [
+			{id: 'red', height: 35, width: 35, left: 37, top: 25, canBeLong: false, isColor: true},
+			{id: 'up', height: 40, width: 40, left: 77, top: 20, canBeLong: false, isArrow: true},
+			{id: 'blue', height: 35, width: 35, left: 122, top: 25, canBeLong: false, isColor: true},
+			{id: 'left', height: 40, width: 40, left: 32, top: 65, canBeLong: false, isArrow: true},
+			{title: I('buttons.ok'), id: 'ok', height: 40, width: 40, left: 77, top: 65, canBeLong: false},
+			{id: 'right', height: 40, width: 40, left: 122, top: 65, canBeLong: false, isArrow: true},
+			{id: 'green', height: 35, width: 35, left: 37, top: 110, canBeLong: false, isColor: true},
+			{id: 'down', height: 40, width: 40, left: 77, top: 110, canBeLong: false, isArrow: true},
+			{id: 'yellow', height: 35, width: 35, left: 122, top: 110, canBeLong: false, isColor: true},
+			{title: '+', id: 'vol_inc', height: 50, width: 40, left: 200, top: 23, bFontSize: 23, canRepeat: true},
+			{title: '-', id: 'vol_dec', height: 50, width: 40, left: 200, top: 93, bFontSize: 28, canRepeat: true},
+			{title: '+', id: 'prgm_inc', height: 50, width: 40, left: 255, top: 23, bFontSize: 23, canRepeat: false},
+			{title: '-', id: 'prgm_dec', height: 50, width: 40, left: 255, top: 93, bFontSize: 28, canRepeat: false},
+			{title: '1', id: '1', height: 35, width: 45, left: 20, top: 330, canBeLong: true},
+			{title: '2', id: '2', height: 35, width: 45, left: 75, top: 330, canBeLong: true},
+			{title: '3', id: '3', height: 35, width: 45, left: 130, top: 330, canBeLong: true},
+			{title: '4', id: '4', height: 35, width: 45, left: 20, top: 375, canBeLong: true},
+			{title: '5', id: '5', height: 35, width: 45, left: 75, top: 375, canBeLong: true},
+			{title: '6', id: '6', height: 35, width: 45, left: 130, top: 375, canBeLong: true},
+			{title: '7', id: '7', height: 35, width: 45, left: 20, top: 420, canBeLong: true},
+			{title: '8', id: '8', height: 35, width: 45, left: 75, top: 420, canBeLong: true},
+			{title: '9', id: '9', height: 35, width: 45, left: 130, top: 420, canBeLong: true},
+			{title: ' ←', id: 'back', height: 35, width: 45, left: 20, top: 465, bFontSize: 23, canBeLong: false},
+			{title: '0', id: '0', height: 35, width: 45, left: 75, top: 465, canBeLong: true },
+			{title: '↻', id: 'swap', height: 35, width: 45, left: 130, top: 465, canBeLong: true},
+			
+			{id: 'power', height: 35, width: 95, left: 200, top: 330, canBeLong: false},
+			{id: 'home', height: 35, width: 95, left: 200, top: 375, canBeLong: true},
+			{title: I('buttons.mute'), id: 'mute', height: 35, width: 95, left: 200, top: 420, canBeLong: false},
+			
+			{title: I('buttons.list'), id: 'list', height: 35, width: 133, top: 522, left: 20},
+			{title: I('buttons.hdd'), id: 'mail', height: 35, width: 134, top: 522, left: 162, isLong: true},
+			{title: I('buttons.epg'), id: 'epg', height: 35, width: 133, top: 563, left: 20},
+			{title: I('buttons.pip'), id: 'pip', height: 35, width: 134, top: 563, left: 162},
+			{title: I('buttons.mail'), id: 'mail', height: 35, width: 133, top: 604, left: 20},
+			{title: I('buttons.aux'), id: 'tv', height: 35, width: 134, top: 604, left: 162},
+			
+			{id: 'rec', height: 30, width: 32, top: 660, left: 20},
+			{title: '«', id: 'prev', height: 30, width: 32, top: 660, left: 61, bFontSize: 25},
+			{title: '‹', id: 'bwd', height: 30, width: 32, top: 660, left: 102, bFontSize: 27},
+			{id: 'play', height: 30, width: 32, top: 660, left: 142, bFontSize: 13},
+			{id: 'stop', height: 30, width: 32, top: 660, left: 183},
+			{title: '›', id: 'fwd', height: 30, width: 32, top: 660, left: 225, bFontSize: 27},
+			{title: '»', id: 'next', height: 30, width: 32, top: 660, left: 264, bFontSize: 25}
+		];
+		
+		var view_trackpad = Ti.UI.createView({
+			backgroundGradient: {
+				type: 'linear',
+				colors: [{
+					color: '#868686',
+					offset: 0
+				}, {
+					color: '#818181',
+					offset: 0.25
+				}, {
+					color: '#6d6d6d',
+					offset: 1
+				}]
+			},
+			borderColor: 'gray',
+			borderRadius: 5,
+			top: 170,
+			left: 20,
+			right: 20,
+			height: 140,
+			zIndex: 5
+		});
+
+		view_trackpad.addEventListener('swipe', function(e) {
+			RequestHandler.callKey(e.direction, false);
+		});
+		
+		view_trackpad.addEventListener('click', function(e) {
+			RequestHandler.callKey('ok', false);
+		});
+		
+		win.add(view_trackpad);
 	}
 	
 	tabbedBar.addEventListener('click', function(e) {
@@ -292,6 +376,33 @@
 					image: '/img/arrow_' + button.id + '.png',
 					height: 20,
 					width: 20
+				});
+				
+				button.add(img_button);
+			} else if(button.id == 'play') {
+				var img_button = Ti.UI.createImageView({
+					image: '/img/play_pause.png',
+					height: 20,
+					width: 20,
+					top: 6
+				});
+				
+				button.add(img_button);
+			} else if(button.id == 'rec') {
+				var img_button = Ti.UI.createImageView({
+					image: '/img/rec.png',
+					height: 20,
+					width: 20,
+					top: 6
+				});
+				
+				button.add(img_button);
+			} else if(button.id == 'stop') {
+				var img_button = Ti.UI.createImageView({
+					image: '/img/stop.png',
+					height: 20,
+					width: 20,
+					top: 6
 				});
 				
 				button.add(img_button);
